d28013d438acbd8a02689e9ca0af74c6b2d8543a
myinfo <chetraj0101@gmail.com>
2023-06-20 18:24:47 -0500

    change html to show burger with toogleNav script file

--- src/app/headersection/headersection.component.ts
+++ src/app/headersection/headersection.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./headersection.component.css']
 })
 export class HeadersectionComponent {
+  navIsActive = false;
 
+  toggleNav() {
+    this.navIsActive = !this.navIsActive;
+  }
 }
